088fa495b2669d0e1ca59ef46ae8647f9297e533
unknown <ksekwamote@gmail.com>
2020-08-05 00:13:23 +0200

    Updates

---
+++ precache-manifest.6276edb8e03e827ee35504dd28d2c698.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2537d0a14ea475e2f298561a6ea32cb",
+    "url": "/cvmaker_new/index.html"
+  },
+  {
+    "revision": "e9d443b73aeedce5aea9",
+    "url": "/cvmaker_new/static/css/main.2368002a.chunk.css"
+  },
+  {
+    "revision": "9a53190f4672bff31510",
+    "url": "/cvmaker_new/static/js/2.7b4d71fb.chunk.js"
+  },
+  {
+    "revision": "e564c7b108a4a9450851db219fac0a35",
+    "url": "/cvmaker_new/static/js/2.7b4d71fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d443b73aeedce5aea9",
+    "url": "/cvmaker_new/static/js/main.cfd6bedc.chunk.js"
+  },
+  {
+    "revision": "d8bad004aa07ecc4aee7",
+    "url": "/cvmaker_new/static/js/runtime-main.6cc14cd2.js"
+  },
+  {
+    "revision": "a8cbcc46d074694fbaf0ddd9ae935543",
+    "url": "/cvmaker_new/static/media/banner-bg.a8cbcc46.png"
+  },
+  {
+    "revision": "87b7cecd94ae076c2a7140c321e4452b",
+    "url": "/cvmaker_new/static/media/flexslider-icon.87b7cecd.svg"
+  },
+  {
+    "revision": "9c9cb7a6055043933ba68854f521af45",
+    "url": "/cvmaker_new/static/media/flexslider-icon.9c9cb7a6.eot"
+  },
+  {
+    "revision": "b4c9e5057989b9727a5df4e0a21af33c",
+    "url": "/cvmaker_new/static/media/flexslider-icon.b4c9e505.ttf"
+  },
+  {
+    "revision": "f8b92f66539473eea649c8514eb836a0",
+    "url": "/cvmaker_new/static/media/flexslider-icon.f8b92f66.woff"
+  },
+  {
+    "revision": "139e74e298bca37a25d2bd5868552e04",
+    "url": "/cvmaker_new/static/media/fontawesome-webfont.139e74e2.svg"
+  },
+  {
+    "revision": "706450d7bba6374ca02fe167d86685cb",
+    "url": "/cvmaker_new/static/media/fontawesome-webfont.706450d7.ttf"
+  },
+  {
+    "revision": "97493d3f11c0a3bd5cbd959f5d19b699",
+    "url": "/cvmaker_new/static/media/fontawesome-webfont.97493d3f.woff2"
+  },
+  {
+    "revision": "d9ee23d59d0e0e727b51368b458a0bff",
+    "url": "/cvmaker_new/static/media/fontawesome-webfont.d9ee23d5.woff"
+  },
+  {
+    "revision": "f7c2b4b747b1a225eb8dee034134a1b0",
+    "url": "/cvmaker_new/static/media/fontawesome-webfont.f7c2b4b7.eot"
+  },
+  {
+    "revision": "ca6bd50e224f6590aa3a3c468abc4eef",
+    "url": "/cvmaker_new/static/media/left-image.ca6bd50e.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/cvmaker_new/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "6077d773d2240d88402b3c8e531c9fb9",
+    "url": "/cvmaker_new/static/media/right-image.6077d773.png"
+  },
+  {
+    "revision": "2b168a8a4272e95a3ef6d8d4a22acbe2",
+    "url": "/cvmaker_new/static/media/slider-icon.2b168a8a.png"
+  },
+  {
+    "revision": "9c025b5e94566ed7d2909e6b3e2e5451",
+    "url": "/cvmaker_new/static/media/template 1.9c025b5e.png"
+  },
+  {
+    "revision": "9dd3425abf707ca9ca476555f1c4d579",
+    "url": "/cvmaker_new/static/media/template 2.9dd3425a.jpg"
+  },
+  {
+    "revision": "fa60c1e5b7dfbfda30bae52e5ffc4b95",
+    "url": "/cvmaker_new/static/media/template 3.fa60c1e5.png"
+  },
+  {
+    "revision": "9edaf8d4b9ac61483b51b56d17a11357",
+    "url": "/cvmaker_new/static/media/template 4.9edaf8d4.jpg"
+  }
+]);
