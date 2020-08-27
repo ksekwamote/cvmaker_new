4f764bcbd9c3930c53cdba0db9e55d0adfec014a
kutlo sekwamote <ksekwamote@gmail.com>
2020-08-27 16:16:33 +0200

    Heroku trial 3

--- server/server.js
+++ server/server.js
@@ -140,6 +140,9 @@ app.post('/create-pdf' , (req, res)=>{
 
 if(process.env.NODE_ENV === "production"){
 	app.use(express.static("build"))
+	app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index.html')); // relative path
+    });
 }
 
 
