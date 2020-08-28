eb8594c746ca12bae528b4b8eda9de1bd69774c4
kutlo sekwamote <ksekwamote@gmail.com>
2020-08-28 15:30:46 +0200

    server edit

--- server.js
+++ server.js
@@ -141,9 +141,9 @@ app.post('/create-pdf' , (req, res)=>{
 
 if(process.env.NODE_ENV === "production"){
 	app.use(express.static("client/build"))
-	//app.get('*', (req, res) => {
-     //   res.sendFile(path.join(__dirname,'client', 'build', 'index.html')); // relative path
-    //});
+	app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname,'client', 'build', 'index.html')); // relative path
+    });
 }
 
 
