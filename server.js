922bc088d50fafee2f7bcadcdd039b4c40d26553
kutlo sekwamote <ksekwamote@gmail.com>
2020-08-28 15:33:45 +0200

    server

--- server.js
+++ server.js
@@ -139,12 +139,9 @@ app.post('/create-pdf' , (req, res)=>{
 	})
 });
 
-if(process.env.NODE_ENV === "production"){
+
 	app.use(express.static("client/build"))
-	app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname,'client', 'build', 'index.html')); // relative path
-    });
-}
+	
 
 
 app.listen(port , () => console.log(`Listen to port ${port}`))
