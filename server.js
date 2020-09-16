cda690ca7bc65104aed756f533730e7fb0de5d13
kutlo sekwamote <ksekwamote@gmail.com>
2020-09-16 14:43:01 +0200

    router problem

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.get('/download', (req,res) => {
 })
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/public/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
